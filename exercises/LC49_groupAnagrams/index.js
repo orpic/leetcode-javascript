1ab11ac5a20f8d9b13dd1f21181fe59c2e0b7b36
shobhit-u <shobhitumrao5@gmail.com>
2023-02-05 19:12:21 +0530

    LC49_groupAnagrams

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,18 @@
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+  let grouped = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    // const strs[i] = strs[i];
+
+    const key = strs[i].split("").sort().join("");
+
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+
+    grouped[key].push(strs[i]);
+  }
+  return Object.values(grouped);
+}
 
 module.exports = groupAnagrams;
