5097d7f83bb45d23647a1bde2d8fd2caf0164f28
shobhit-u <shobhitumrao5@gmail.com>
2023-02-05 18:19:28 +0530

    LC242_isAnagram

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,22 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+  // checking if lengths are equal or not
+
+  if (s.length !== t.length) return false;
+
+  const sCharMap = {};
+
+  for (let i = 0; i < s.length; i++) {
+    sCharMap[s[i]] = sCharMap[s[i]] + 1 || 1;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (!sCharMap[t[i]]) {
+      return false;
+    } else {
+      sCharMap[t[i]]--;
+    }
+  }
+  return true;
+}
 
 module.exports = isAnagram;
