66734d1637d8327c116335672c0bf0e389001499
shobhit-u <shobhitumrao5@gmail.com>
2023-02-06 01:01:58 +0530

    LC55_canJump

--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,3 +1,17 @@
-function canJump(nums) {}
+function canJump(nums) {
+  //assigning the last postion a goalpost
+  let goalPost = nums.length - 1;
+
+  // moving the goalPost if the previous index
+  // can reach and setting the previous index as gal post
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i + nums[i] >= goalPost) {
+      goalPost = i;
+    }
+  }
+
+  if (goalPost === 0) return true;
+  if (goalPost !== 0) return false;
+}
 
 module.exports = canJump;
