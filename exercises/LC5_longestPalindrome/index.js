6239eb8bfa9fa45f45686c86dbed06eb609a9e19
shobhit-u <shobhitumrao5@gmail.com>
2023-02-05 15:50:51 +0530

    LC5_longestPalindrome

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,28 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  let initialPosition = 0;
+  let maxLength = 1;
+
+  function expandPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentLength = right - left + 1;
+      if (currentLength > maxLength) {
+        maxLength = currentLength;
+        initialPosition = left;
+      }
+      left--;
+      right++;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandPalindrome(i - 1, i + 1); //odd
+    expandPalindrome(i, i + 1); //even
+  }
+
+  return s.slice(initialPosition, initialPosition + maxLength);
+}
 
 module.exports = longestPalindrome;
