192ccebdbac9f8ae5e2b5851547ab4ea81a847ec
shobhit-u <shobhitumrao5@gmail.com>
2023-02-05 19:51:02 +0530

    LC20_isValid

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,23 @@
-const isValid = s => {};
+const isValid = (s) => {
+  let stack = [];
+
+  let bracketPairMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+
+    if (bracketPairMap[char]) {
+      stack.push(char);
+    } else if (bracketPairMap[stack.pop()] !== char) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
 
 module.exports = isValid;
