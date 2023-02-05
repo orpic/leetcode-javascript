9c167c307b5bdf488a0e89bd83e8e4f4f334ecb8
shobhit-u <shobhitumrao5@gmail.com>
2023-02-05 17:55:48 +0530

    LC3_lengthOfLongestSubstrting

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,37 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  //key = []character
+  //value = the index of the last time that []character appeared
+  //        in our input string as we are iterate through it
+  let windowMap = {};
+
+  let startOfWindow = 0;
+  let endOfWindow = 0; //for loop
+  let maxLength = 0;
+
+  for (endOfWindow; endOfWindow < s.length; endOfWindow++) {
+    const lastChar = s[endOfWindow];
+
+    // this if comes first in code but when running it in mind simulation
+    // we will not cross this code until we have a duplicate value
+
+    //this is checking if we prev have that char in our window
+    // if yes then we have to update the start of window to a place where
+    // the one more than the where last char of that window was found
+    if (windowMap[lastChar] >= startOfWindow) {
+      //start of the window to be one index above the last time A was found
+      startOfWindow = windowMap[lastChar] + 1;
+    }
+
+    windowMap[lastChar] = endOfWindow;
+    //key value pair
+    //last time the character appeared in our string as we iterate through it
+
+    maxLength = Math.max(maxLength, endOfWindow - startOfWindow + 1);
+  }
+  return maxLength;
+}
 
 module.exports = lengthOfLongestSubstring;
