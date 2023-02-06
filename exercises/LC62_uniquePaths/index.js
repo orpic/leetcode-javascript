7e77063706282bead0cc97b39f572d300f029071
shobhit-u <shobhitumrao5@gmail.com>
2023-02-06 23:40:49 +0530

    LC62_uniquePaths

--- exercises/LC62_uniquePaths/index.js
+++ exercises/LC62_uniquePaths/index.js
@@ -1,3 +1,25 @@
-function uniquePaths(m, n) {}
+function uniquePaths(m, n) {
+  let dpMatrix = [];
+
+  for (let row = 0; row < m; row++) {
+    dpMatrix.push([]);
+  }
+
+  for (let row = 0; row < m; row++) {
+    dpMatrix[row][0] = 1;
+  }
+
+  for (let col = 0; col < n; col++) {
+    dpMatrix[0][col] = 1;
+  }
+
+  for (let row = 1; row < m; row++) {
+    for (let col = 1; col < n; col++) {
+      dpMatrix[row][col] = dpMatrix[row - 1][col] + dpMatrix[row][col - 1];
+    }
+  }
+
+  return dpMatrix[m - 1][n - 1];
+}
 
 module.exports = uniquePaths;
