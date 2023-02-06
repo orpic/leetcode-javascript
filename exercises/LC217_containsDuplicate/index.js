a8a567cbcbee69579e7ae4787e08afa655abfebb
shobhit-u <shobhitumrao5@gmail.com>
2023-02-07 00:05:00 +0530

    LC217_containsDuplicate

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,17 @@
-const containsDuplicate = nums => {};
+const containsDuplicate = (nums) => {
+  let visitedNumbers = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if (visitedNumbers[num]) {
+      return true;
+    } else {
+      visitedNumbers[num] = true;
+    }
+  }
+
+  return false;
+};
 
 module.exports = containsDuplicate;
