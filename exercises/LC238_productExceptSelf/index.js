a9d11e8ea110c62cc04e2f0d08fc0e7fa56ae2ac
shobhit-u <shobhitumrao5@gmail.com>
2023-02-09 20:50:42 +0530

    LC238_productExceptSelf

--- exercises/LC238_productExceptSelf/index.js
+++ exercises/LC238_productExceptSelf/index.js
@@ -1,3 +1,24 @@
-const productExceptSelf = nums => {};
+const productExceptSelf = (nums) => {
+  // mulitplying the left part of array and then the right
+  // and then multiplying them together to achieve this without
+  let output = nums.map((n) => 1);
+  let product = 1;
+
+  //the multiplication  from left
+  for (let i = 0; i < nums.length; i++) {
+    output[i] = output[i] * product;
+    product = product * nums[i];
+  }
+
+  product = 1;
+  // the multiplication from right
+
+  for (let j = nums.length - 1; j >= 0; j--) {
+    output[j] = output[j] * product;
+    product = product * nums[j];
+  }
+
+  return output;
+};
 
 module.exports = productExceptSelf;
