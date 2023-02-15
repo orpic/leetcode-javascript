642dfda611dfa9913ba4ae8baa1da7fc1a3598fd
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-02-15 22:29:17 +0530

    LC1_twoSum

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -6,6 +6,26 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,5,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+function twoSum(arr, target) {
+  // we could take an array but js object has a lookup time of O(1)
+  // when compared to lookup time of an array whih is O(n)
+  // { 2: 0 , 7:0}
+  const numVisited = {};
+  const resultArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    const compliment = target - num;
+
+    if (numVisited[compliment] !== undefined) {
+      resultArray.push(i);
+      resultArray.push(numVisited[compliment]);
+    }
+
+    numVisited[num] = i;
+  }
+
+  return resultArray;
+}
 
 module.exports = twoSum;
