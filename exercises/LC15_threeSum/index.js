0e13f58c5791738957293898ca72a17d7cf82f13
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-02-17 22:39:25 +0530

    LC15_threeSum

--- exercises/LC15_threeSum/index.js
+++ exercises/LC15_threeSum/index.js
@@ -1,3 +1,37 @@
-const threeSum = nums => {};
+const threeSum = (nums) => {
+  //sorting array in ascending order
+  nums.sort((a, b) => a - b);
+  //   the result array
+  let res = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const target = 0 - nums[i];
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+      // here we skip the part where duplcate array might occur
+    }
+
+    while (left < right) {
+      if (nums[left] + nums[right] === target) {
+        res.push([nums[i], nums[left], nums[right]]);
+        left++;
+        // here we skip the part where duplcate array might occur
+        while (nums[left] === nums[left - 1]) {
+          left++;
+        }
+      } else if (nums[left] + nums[right] < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return res;
+};
 
 module.exports = threeSum;
