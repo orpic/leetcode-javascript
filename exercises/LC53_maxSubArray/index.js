e0577eae95e10afd5db09c0629be189da6add2ec
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-02-18 23:26:38 +0530

    LC53_maxSubArray

--- exercises/LC53_maxSubArray/index.js
+++ exercises/LC53_maxSubArray/index.js
@@ -1,3 +1,17 @@
-function maxSubArray(nums) {}
+function maxSubArray(nums) {
+  let dp = [nums[0]];
+
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+
+    dp[i] = Math.max(num, num + dp[i - 1]);
+
+    max = Math.max(max, dp[i]);
+  }
+
+  return max;
+}
 
 module.exports = maxSubArray;
