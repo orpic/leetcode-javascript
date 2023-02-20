ad7dd594b58da1c0d9b1abd20f030ea01ce1b93f
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-02-20 22:45:16 +0530

    LC153_findMin

--- exercises/LC153_findMin/index.js
+++ exercises/LC153_findMin/index.js
@@ -1,3 +1,36 @@
-function findMin(nums) {}
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  if (nums.length === 1) {
+    return nums[0];
+  }
+
+  if (nums[left] < nums[right]) {
+    return nums[left];
+  }
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    const leftVal = nums[left];
+    const midVal = nums[mid];
+
+    const leftOfMid = nums[mid - 1];
+    const rightOfMid = nums[mid + 1];
+
+    if (midVal > rightOfMid) {
+      return rightOfMid;
+    } else if (leftOfMid > midVal) {
+      return midVal;
+    }
+
+    if (midVal > leftVal) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+}
 
 module.exports = findMin;
