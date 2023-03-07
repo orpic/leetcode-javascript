0567739b3dee07bcf0dc485cc9ca10f7f40cc4e3
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-07 23:20:18 +0530

    LC33_search

--- exercises/LC33_search/index.js
+++ exercises/LC33_search/index.js
@@ -1,3 +1,57 @@
-function search(nums, target) {}
+function findInflextionPoint(rotatedSortedArray) {
+  let left = 0;
+  let right = rotatedSortedArray.length - 1;
+
+  if (rotatedSortedArray.length === 1) {
+    return 0;
+  }
+
+  if (rotatedSortedArray[left] < rotatedSortedArray[right]) {
+    return 0;
+  }
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (rotatedSortedArray[mid] < rotatedSortedArray[mid - 1]) {
+      return mid;
+    }
+
+    if (rotatedSortedArray[mid] > rotatedSortedArray[mid + 1]) {
+      return mid + 1;
+    }
+
+    if (rotatedSortedArray[mid] < rotatedSortedArray[left]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+}
+
+function binarySearch(nums, target, left, right) {
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+function search(nums, target) {
+  const inflecIndex = findInflextionPoint(nums);
+
+  const left = binarySearch(nums, target, 0, inflecIndex - 1);
+  const right = binarySearch(nums, target, inflecIndex, nums.length - 1);
+
+  return Math.max(left, right);
+}
 
 module.exports = search;
