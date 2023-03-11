98e0490840215ed9f8d5fcb73d50ec5c7b625526
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-12 00:44:03 +0530

    LC56_merge

--- exercises/LC56_merge/index.js
+++ exercises/LC56_merge/index.js
@@ -1,3 +1,23 @@
-const merge = intervals => {};
+const merge = (intervals) => {
+  if (!intervals.length) return intervals;
+
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const res = [intervals[0]];
+
+  for (let i = 0; i < intervals.length; i++) {
+    const currentInterval = intervals[i];
+
+    const lastInterval = res[res.length - 1];
+
+    if (currentInterval[0] <= lastInterval[1]) {
+      lastInterval[1] = Math.max(currentInterval[1], lastInterval[1]);
+    } else {
+      res.push(currentInterval);
+    }
+  }
+
+  return res;
+};
 
 module.exports = merge;
