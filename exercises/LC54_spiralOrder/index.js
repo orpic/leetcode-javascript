49f5f15a195f393ec3f1d593456a86e18d28cd94
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-14 23:09:06 +0530

    LC54_spiralOrder

--- exercises/LC54_spiralOrder/index.js
+++ exercises/LC54_spiralOrder/index.js
@@ -1,3 +1,44 @@
-function spiralOrder(matrix) {}
+function spiralOrder(matrix) {
+  const spiralArray = [];
+
+  if (matrix.length === 0) return spiralArray;
+
+  let top = 0;
+  let bottom = matrix.length - 1;
+  let left = 0;
+  let right = matrix[0].length - 1;
+
+  let dir = "right";
+
+  while (top <= bottom && left <= right) {
+    if (dir === "right") {
+      for (let i = left; i <= right; i++) {
+        spiralArray.push(matrix[top][i]);
+      }
+      top++;
+      dir = "down";
+    } else if (dir === "down") {
+      for (let i = top; i <= bottom; i++) {
+        spiralArray.push(matrix[i][right]);
+      }
+      right--;
+      dir = "left";
+    } else if (dir === "left") {
+      for (let i = right; i >= left; i--) {
+        spiralArray.push(matrix[bottom][i]);
+      }
+      bottom--;
+      dir = "up";
+    } else if (dir === "up") {
+      for (let i = bottom; i >= top; i--) {
+        spiralArray.push(matrix[i][left]);
+      }
+      left++;
+      dir = "right";
+    }
+  }
+
+  return spiralArray;
+}
 
 module.exports = spiralOrder;
