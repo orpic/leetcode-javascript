6dfe5fdf65a7b105e716fa80125f5dda0b91b748
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-15 23:02:26 +0530

    LC73_setZeroes

--- exercises/LC73_setZeroes/index.js
+++ exercises/LC73_setZeroes/index.js
@@ -1,3 +1,48 @@
-function setZeroes(matrix) {}
+function setZeroes(matrix) {
+  let firstColHasZero = false;
+
+  let firstRowHasZero = false;
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
+      break;
+    }
+  }
+  for (let i = 0; i < matrix[0].length; i++) {
+    if (matrix[0][i] === 0) {
+      firstRowHasZero = true;
+      break;
+    }
+  }
+
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][col] === 0) {
+        matrix[0][col] = 0;
+        matrix[row][0] = 0;
+      }
+    }
+  }
+
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][0] === 0 || matrix[0][col] === 0) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+
+  if (firstColHasZero) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+  if (firstRowHasZero) {
+    for (let i = 0; i < matrix[0].length; i++) {
+      matrix[0][i] = 0;
+    }
+  }
+}
 
 module.exports = setZeroes;
