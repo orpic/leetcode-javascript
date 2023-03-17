9422098d6cbe9cb4a244196b605a5d623e21f221
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-17 23:32:31 +0530

    LC206_reverseList

--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,3 +1,16 @@
-const reverseList = head => {};
+const reverseList = (head) => {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let temp = current.next;
+    current.next = prev;
+    prev = current;
+
+    current = temp;
+  }
+
+  return prev;
+};
 
 module.exports = reverseList;
