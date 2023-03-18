715a61efcd8cdeffb4cd26caac553b9df733ee84
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-18 22:59:31 +0530

    LC141_hasCycle

--- exercises/LC141_hasCycle/index.js
+++ exercises/LC141_hasCycle/index.js
@@ -1,3 +1,16 @@
-function hasCycle(head) {}
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = hasCycle;
