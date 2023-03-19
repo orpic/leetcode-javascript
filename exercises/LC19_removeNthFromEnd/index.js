ad8bce2e07c7bc0d049f5996ed4003f7a73386ce
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-19 23:50:27 +0530

    LC19_removeNthFromEnd

--- exercises/LC19_removeNthFromEnd/index.js
+++ exercises/LC19_removeNthFromEnd/index.js
@@ -1,3 +1,20 @@
-function removeNthFromEnd(head, n) {}
+function removeNthFromEnd(head, n) {
+  let dummyHead = { next: head };
+  let slow = dummyHead;
+  let fast = dummyHead;
+
+  for (let i = 1; i <= n; i++) {
+    fast = fast.next;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  slow.next = slow.next.next;
+
+  return dummyHead.next;
+}
 
 module.exports = removeNthFromEnd;
