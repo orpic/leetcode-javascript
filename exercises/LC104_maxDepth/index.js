52152369bd1ea888eb129a9ccbb126dafe19f7bb
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-23 23:51:30 +0530

    LC104_maxDepth

--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,3 +1,18 @@
-function maxDepth(root) {}
+function maxDepth(root) {
+  let maxDepth = 0;
+
+  function dive(node, currentDepth) {
+    if (!node) {
+      maxDepth = Math.max(currentDepth - 1, maxDepth);
+      return;
+    }
+
+    dive(node.left, currentDepth + 1);
+    dive(node.right, currentDepth + 1);
+  }
+
+  dive(root, 1);
+  return maxDepth;
+}
 
 module.exports = maxDepth;
