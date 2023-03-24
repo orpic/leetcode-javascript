7ed215f77f1a77d336667b9ccd942898a35a5510
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-24 23:05:36 +0530

    LC102_levelOrder

--- exercises/LC102_levelOrder/index.js
+++ exercises/LC102_levelOrder/index.js
@@ -1,3 +1,22 @@
-function levelOrder(root) {}
+function levelOrder(root) {
+  const result = [];
+
+  function helperFunc(node, depth) {
+    if (!node) return;
+
+    if (!result[depth]) {
+      result[depth] = [];
+    }
+
+    result[depth].push(node.val);
+
+    helperFunc(node.left, depth + 1);
+    helperFunc(node.right, depth + 1);
+  }
+
+  helperFunc(root, 0);
+
+  return result;
+}
 
 module.exports = levelOrder;
