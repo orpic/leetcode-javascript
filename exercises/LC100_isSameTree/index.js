dd20888ec94a36a214c50728e2ffac6f5a5fe951
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-25 21:58:30 +0530

    LC100_levelOrder

--- exercises/LC100_isSameTree/index.js
+++ exercises/LC100_isSameTree/index.js
@@ -1,3 +1,26 @@
-function isSameTree(p, q) {}
+function isSameTree(p, q) {
+  let sameTree = true;
+
+  function checkSameNode(p, q) {
+    // base case
+    if (!p && !q) {
+      return;
+    } else if (!p || !q) {
+      // one node is null and other may be a tree they are not equal
+      sameTree = false;
+      return;
+    } else if (p.val !== q.val) {
+      sameTree = false;
+      return;
+    }
+
+    checkSameNode(p.left, q.left);
+    checkSameNode(p.right, q.right);
+  }
+
+  checkSameNode(p, q);
+
+  return sameTree;
+}
 
 module.exports = isSameTree;
