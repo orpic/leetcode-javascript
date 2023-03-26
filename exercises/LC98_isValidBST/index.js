d387e63b45ca2a72cfd7519b5144b5392df0b788
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-26 22:14:33 +0530

    LC98_isValidBST

--- exercises/LC98_isValidBST/index.js
+++ exercises/LC98_isValidBST/index.js
@@ -1,3 +1,24 @@
-function isValidBST(root) {}
+function isValidBST(root) {
+  let isValid = true;
+
+  function helper(node, min, max) {
+    if (!node) return;
+
+    if (
+      (min !== null && node.val <= min) ||
+      (max !== null && node.val >= max)
+    ) {
+      isValid = false;
+      return;
+    }
+
+    helper(node.left, min, node.val);
+    helper(node.right, node.val, max);
+  }
+
+  helper(root, null, null);
+
+  return isValid;
+}
 
 module.exports = isValidBST;
