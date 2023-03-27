84f3a3fc5e4163f08626fa27e117c87fcb39569b
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-27 22:57:05 +0530

    LC235_lowestCommonAncestor

--- exercises/LC235_lowestCommonAncestor/index.js
+++ exercises/LC235_lowestCommonAncestor/index.js
@@ -1,3 +1,21 @@
-function lowestCommonAncestor(root, p, q) {}
+function lowestCommonAncestor(root, p, q) {
+  const pVal = p.val;
+
+  const qVal = q.val;
+
+  let currentNode = root;
+
+  while (currentNode) {
+    const currentVal = currentNode.val;
+
+    if (currentVal < pVal && currentVal < qVal) {
+      currentNode = currentNode.right;
+    } else if (currentVal > pVal && currentVal > qVal) {
+      currentNode = currentNode.left;
+    } else {
+      return currentNode;
+    }
+  }
+}
 
 module.exports = lowestCommonAncestor;
